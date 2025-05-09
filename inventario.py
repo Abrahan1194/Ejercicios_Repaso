59f21b741c0cab0498254e01916be340d3ee39bb
Abrahan1194 <abrahan.1194@gmail.com>
2025-05-09 16:44:41 -0500

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -1,113 +1,134 @@
-inventario = {}  # Aquí guardamos los productos y su información
+# Inicializa un diccionario vacio para almacenar el inventario.
+# Cada producto sera una clave en el diccionario, y su valor sera otro diccionario con 'precio' y 'cantidad'.
+inventario = {}
 
-def agregar_producto(inventario_local):
-    nombre = input("Ingrese el nombre del producto: ").strip()  # Pide el nombre del producto
-    if nombre in inventario_local:  # Revisa si el producto ya existe
-        print(f"El producto '{nombre}' ya existe en el inventario.")
-        return  # Sale de la función si el producto ya existe
+def agregar_producto(inventario_local):#definicion de la funcion
+    print("\n\033[94m\033[1mAñadir Producto\033[0m")#imprimir titulo
+    nombre = input("\n\033[1mIngrese el nombre del producto: ").strip()#ingresar nombre del producto(.strip para limpiar espacios)
+    if nombre in inventario_local:#condicional para validar si el usuario ya existe, si existe...
+        print(f"\n\033[93m El producto '{nombre}' ya existe\033[0m")#imprime mensaje que el producto ya existe
+        return#retorna a pedir el nombre del producto
+    while True:#bucle para ingresar precio y sus validaciones
+        try:#try para validar
+            precio_str = input("\n\033[1mIngrese el precio del producto: ").replace(',', '.')#ingresa precio en letras y si ingresa ","los remplaza por "."
+            precio = float(precio_str)#cambia el precio en letras por un valor decimal 
+            if precio < 0:#bucle para validar que el precio sea positivo
+                print("\n\033[93m El precio debe ser un valor positivo.\033[0m")#mensaje que imprime una alerta
+                continue#Salta la iteración actual del bucle y pasa a la siguiente.
+            break#Termina el bucle prematuramente y el control del programa pasa a la siguiente instruccion
+        except ValueError:#captura una excepcion que se produce cuando se le pasa un valor incorrecto a una funcion
+            print("\n\033[93m Ingrese un numero valido para el precio.\033[0m")#imprime instruccion 
 
-    while True:  # Repite hasta que se ingrese un precio válido
-        try:
-            precio_str = input("Ingrese el precio del producto: ").replace(',', '.')  # Pide el precio
-            precio = float(precio_str)  # Convierte el precio a número
-            if precio < 0:
-                print("El precio debe ser un valor positivo.")
-                continue  # Vuelve a pedir el precio si es negativo
-            break  # Sale del bucle si el precio es válido
-        except ValueError:
-            print("Por favor, ingrese un número válido para el precio.")
+    while True:#bucle para ingresar cantidad con sus validaciones
+        try:#empezar validaciones
+            cantidad_str = input("\n\033[1mIngrese la cantidad disponible: ")#pide la cantidad en string
+            cantidad = int(cantidad_str)#cambia cantidad de string a numero entero
+            if cantidad < 0:#condicional para validacion cantidad sea numero positivo
+                print("\n\033[93m La cantidad debe ser un valor positivo.\033[0m")#imprime advertencia
+                continue#Salta la iteración actual del bucle y pasa a la siguiente.
+            break#Termina el bucle prematuramente y el control del programa pasa a la siguiente instruccion
+        except ValueError:#aptura una excepcion que se produce cuando se le pasa un valor incorrecto a una funcion
+            print("\n\033[93m Ingrese un numero entero valido para la cantidad.\033[0m")#imprime mensaje con instruccion correcta
 
-    while True:  # Repite hasta que se ingrese una cantidad válida
-        try:
-            cantidad_str = input("Ingrese la cantidad disponible: ")  # Pide la cantidad
-            cantidad = int(cantidad_str)  # Convierte la cantidad a número entero
-            if cantidad < 0:
-                print("La cantidad debe ser un valor positivo.")
-                continue  # Vuelve a pedir la cantidad si es negativa
-            break  # Sale del bucle si la cantidad es válida
-        except ValueError:
-            print("Por favor, ingrese un número entero válido para la cantidad.")
+    inventario_local[nombre] = {'precio': precio, 'cantidad': cantidad}#en el parametro (que es una blioteca),en la clave nombre guardar los valores precio y cantiad en sus respectivas claves
+    print(f"\n\033[1m\033[32mEl producto '{nombre}' ha sido añadido al inventario.\033[0m")#imprime mensaje si el producto es agregado correctamente 
 
-    inventario_local[nombre] = (precio, cantidad)  # Guarda el nombre, precio y cantidad en el inventario
-    print(f"El producto '{nombre}' ha sido añadido al inventario.")
-
-def consultar_producto(inventario_local):
-    nombre = input("Ingrese el nombre del producto que desea consultar: ").strip()  # Pide el nombre del producto a buscar
-    if nombre in inventario_local:  # Revisa si el producto está en el inventario
-        precio, cantidad = inventario_local[nombre]  # Obtiene el precio y la cantidad del producto
-        print(f"Producto: {nombre}")
-        print(f"Precio: ${precio:,.2f} COP")
-        print(f"Cantidad disponible: {cantidad}")
+def consultar_producto(inventario_local):#definicion funcion para consultar producto 
+    print("\n\033[94m\033[1mConsultar Producto\033[0m")#imprime titulo
+    nombre = input("\n\033[1mIngrese el nombre del producto que desea consultar: ").strip()#ingresar el el nombre del producto a consultar
+    if nombre in inventario_local:#condicional para validar si el usuario si esta en la biblioteca
+        info_producto = inventario_local[nombre]#se crea variable para extraer los datos de biblioteca inventario y asi utilizar su info mas adelante
+        precio = info_producto['precio']#con la variable info_producto se extre la clave precio dela biblioteca nombre
+        cantidad = info_producto['cantidad']#con la variable info_producto se extre la clave cantidad dela biblioteca nombre
+        print("\n")#espacio
+        print("\033[1m" + "-" * 30 + "\033[0m")#marquetado
+        print(f"\033[1m| \033[94m{'Producto':<15} \033[0m| {nombre:<12} |\033[0m")#imprime producto y nombre
+        print("\033[1m" + "-" * 30 + "\033[0m")#marquetado
+        print(f"\033[1m| \033[94m{'Precio':<15}\033[0m | ${precio:11,.2f} |\033[0m")#imprime precio
+        print("\033[1m" + "-" * 30 + "\033[0m")#marquetado
+        print(f"\033[1m|\033[94m {'Cantidad':<15}\033[0m | {cantidad:<12} |\033[0m")#imprime cantidad 
+        print("\033[1m" + "-" * 30 + "\033[0m")#marquetado
     else:
-        print(f"El producto '{nombre}' no se encuentra en el inventario.")
+        print(f"\033[93m El producto '{nombre}' no se encuentra en el inventario.\033[0m")#si el productno no esta en la biblioteca imprime mensaje de que no esta 
 
-def actualizar_precio(inventario_local):
-    nombre = input("Ingrese el nombre del producto cuyo precio desea actualizar: ").strip()  # Pide el nombre del producto a actualizar
-    if nombre in inventario_local:  # Revisa si el producto está en el inventario
-        while True:  # Repite hasta que se ingrese un precio válido
-            try:
-                nuevo_precio_str = input("Ingrese el nuevo precio: ").replace(',', '.')  # Pide el nuevo precio
-                nuevo_precio = float(nuevo_precio_str)  # Convierte el nuevo precio a número
-                if nuevo_precio < 0:
-                    print("El precio debe ser un valor positivo.")
-                    continue  # Vuelve a pedir el precio si es negativo
-                inventario_local[nombre] = (nuevo_precio, inventario_local[nombre][1])  # Actualiza el precio del producto
-                print(f"El precio de '{nombre}' ha sido actualizado a ${nuevo_precio:,.2f} COP.")
-                break  # Sale del bucle si el precio es válido
+def actualizar_precio(inventario_local):#definicion funcion variable
+    print("\n\033[94m\033[1mActualizar Precio\033[0m")#imprime titulo
+    nombre = input("\n\033[1mIngrese el nombre del producto cuyo precio quiere actualizar: ").strip()#ingresa nombre del producto a actualizar
+    if nombre in inventario_local:#condicional de validacion
+        while True:#bucle para actulixar y sus validaciones 
+            try:#empieza validacion
+                nuevo_precio_str = input("\n\033[1mIngrese el nuevo precio: ").replace(',', '.')#ingresa nuevo precio en string y cambia ","por puntos
+                nuevo_precio = float(nuevo_precio_str)#cambia string por decimal
+                if nuevo_precio < 0:#condicional para verificar si el numero es positivo
+                    print("\n\033[93mEl precio debe ser un valor positivo.\033[0m")#si no es positivo imprime este mensaje 
+                    continue#
+                inventario_local[nombre]['precio'] = nuevo_precio
+                print(f"\n\033[1mEl precio de '{nombre}' ha sido actualizado a ${nuevo_precio:,.2f}.\033[0m")
+                break
             except ValueError:
-                print("Por favor, ingrese un número válido para el precio.")
+                print("\n\033[93mPor favor, ingrese un numero valido para el precio.\033[0m")
     else:
-        print(f"El producto '{nombre}' no se encuentra en el inventario.")
+        print(f"\n\033[93m El producto '{nombre}' no se encuentra en el inventario.\033[0m")
+
 
 def eliminar_producto(inventario_local):
-    nombre = input("Ingrese el nombre del producto que desea eliminar: ").strip()  # Pide el nombre del producto a eliminar
-    if nombre in inventario_local:  # Revisa si el producto está en el inventario
-        del inventario_local[nombre]  # Elimina el producto del inventario
-        print(f"El producto '{nombre}' ha sido eliminado del inventario.")
+    print("\n\033[94m\033[1mEliminar Producto\033[0m")
+    nombre = input("\033[1mIngrese el nombre del producto que quiere eliminar: ").strip()
+    if nombre in inventario_local:
+        del inventario_local[nombre]
+        print(f"\n\033[1mEl producto '{nombre}' ha sido eliminado.\033[0m")
     else:
-        print(f"El producto '{nombre}' no se encuentra en el inventario.")
+        print(f"\n\033[93m El producto no se encuentra en el inventario.\033[0m")
 
 def calcular_valor_total(inventario_local):
-    valor_total_lambda = lambda inventario: sum(precio * cantidad for precio, cantidad in inventario.values())  # Función para calcular el valor total
-    valor_total = valor_total_lambda(inventario_local)  # Calcula el valor total del inventario
+    print("\n\033[94m\033[1mValor Total del Inventario\033[0m")
+    valor_total_lambda = lambda inventario: sum(producto['precio'] * producto['cantidad'] for producto in inventario.values())
+    valor_total = valor_total_lambda(inventario_local)
+    print(f"\n\033[1mEl valor total del inventario es: ${valor_total:,.2f}\033[0m")
     return valor_total
 
 def mostrar_inventario(inventario_local):
-    if not inventario_local:  # Revisa si el inventario está vacío
-        print("El inventario está vacío.")
+    print("\n\033[94m\033[1mMostrar Inventario\033[0m")
+    if not inventario_local:
+        print("\033[1m\n\033[93m\El inventario está vacio.\033[0m")
         return
-    print("\n--- Inventario Actual ---")
-    for nombre, (precio, cantidad) in inventario_local.items():  # Recorre cada producto en el inventario
-        print(f"Producto: {nombre}, Precio: ${precio:,.2f} COP, Cantidad: {cantidad}")  # Muestra la información del producto
-    print("-\n"*50)
+    print("\033[94m\033[1mInventario Actual   \033[0m")
+    for nombre, info_producto in inventario_local.items():
+        precio = info_producto['precio']
+        cantidad = info_producto['cantidad']
+        print(f"\n\033[1mProducto: {nombre}, Precio: ${precio:,.2f}, Cantidad: {cantidad}\033[0m")
+    print("-" * 35)
 
-print("\nInventario")  # Título del programa
-while True:  # Bucle principal del programa
-    print("\n1. Añadir producto")
-    print("2. Consultar producto")
-    print("3. Actualizar precio")
-    print("4. Eliminar producto")
-    print("5. Calcular valor total del inventario")
-    print("6. Mostrar inventario")
-    print("7. Salir")
+while True:
+    print("\n\033[94m\033[1m" + "═" * 35 + "\033[0m")
+    print("\033[94m\033[1m" + "  SISTEMA DE INVENTARIO  ".center(35) + "\033[0m")
+    print("\033[94m\033[1m" + "═" * 35 + "\033[0m")
+    print("\033[1m  Opciones:\033[0m")
+    print(f"\033[1m  1. Añadir producto{''.ljust(31)}\033[0m")
+    print(f"\033[1m  2. Consultar producto{''.ljust(29)}\033[0m")
+    print(f"\033[1m  3. Actualizar precio{''.ljust(30)}\033[0m")
+    print(f"\033[1m  4. Eliminar producto{''.ljust(30)}\033[0m")
+    print(f"\033[1m  5. Calcular valor total del inventario{''.ljust(24)}\033[0m")
+    print(f"\033[1m  6. Mostrar inventario{''.ljust(27)}\033[0m")
+    print(f"\033[1m  7. Salir{''.ljust(34)}\033[0m")
+    print("\033[94m" + "═" * 35 + "\033[0m")
 
-    opcion = input("Seleccione una opción: ")  # Pide al usuario que elija una opción
+    opcion = input("\033[94m\033[1mSeleccione una opcion: \033[0m")
 
     if opcion == '1':
-        agregar_producto(inventario)  # Llama a la función para agregar un producto
+        agregar_producto(inventario)
     elif opcion == '2':
-        consultar_producto(inventario)  # Llama a la función para consultar un producto
+        consultar_producto(inventario)
     elif opcion == '3':
-        actualizar_precio(inventario)  # Llama a la función para actualizar el precio de un producto
+        actualizar_precio(inventario)
     elif opcion == '4':
-        eliminar_producto(inventario)  # Llama a la función para eliminar un producto
+        eliminar_producto(inventario)
     elif opcion == '5':
-        valor_total = calcular_valor_total(inventario)  # Calcula el valor total del inventario
-        print(f"El valor total del inventario es: ${valor_total:,.2f} COP")
+        valor_total = calcular_valor_total(inventario)
     elif opcion == '6':
-        mostrar_inventario(inventario)  # Llama a la función para mostrar el inventario
+        mostrar_inventario(inventario)
     elif opcion == '7':
-        print("Saliendo del programa. ¡Hasta luego!")
-        break  # Sale del bucle principal y termina el programa
+        print("\n\033[94m\033[1mSaliendo del programa\033[0m")
+        break
     else:
-        print("Opción inválida. Por favor, seleccione una opción válida.")
+        print("\n\033[93m Opcion invalida. seleccione una opcion valida.\033[0m")
